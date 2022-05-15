041d05fd4f79085096255ab074c882838faa35d2
Mark Harfouche <mark.harfouche@gmail.com>
2022-05-15 19:31:42 -0400

    Remove remove the whole xbc dance

--- recipe/build.sh
+++ recipe/build.sh
@@ -1,12 +1,5 @@
 #!/bin/sh
 
-XVFB_RUN=""
-if test `uname` = "Linux"
-then
-  cp -r /usr/include/xcb ${PREFIX}/include/qt
-  XVFB_RUN="xvfb-run -s '-screen 0 640x480x24'"
-fi
-
 pushd sources/shiboken2
 mkdir build && cd build
 
@@ -24,8 +17,3 @@ popd
 ${PYTHON} setup.py dist_info --build-type=shiboken2
 _pythonpath=`${PYTHON} -c "from sysconfig import get_python_version; print(get_python_version())"`
 cp -r shiboken2-5.15.3.dist-info "${PREFIX}"/lib/python"${_pythonpath}"/site-packages/
-
-if test `uname` = "Linux"
-then
-  rm -rf ${PREFIX}/include/qt/xcb
-fi
